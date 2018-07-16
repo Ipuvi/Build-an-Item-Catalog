b0d795682328b70d395f5451876f4b4867982a04
Ipuvi Mishra <ipuvi98@gmail.com>
2018-07-16 15:31:39 +0530

    Templates added...

--- finalProject.py
+++ finalProject.py
@@ -20,45 +20,55 @@ session = DBSession()
 @app.route('/restaurants')
 def showRestaurants():
 	restaurants = session.query(Restaurant).all()
-	return "This page will show all my restaurants..."
+	return render_template('restaurants.html' , restaurants = restaurants)
 
 #Add New Restaurant Page...
-@app.route('/restaurant/new')
+@app.route('/restaurant/new' , methods = ['GET' , 'POST'])
 def newRestaurant():
-	return "This page will be for making a new restaurant..."
+	#return "This page will be for making a new restaurant..."
+	if request.method == 'POST':
+		newrestaurant.name = Restaurant(name = request.form['name'])
+		session.add(newrestaurant)
+		session.commit()
+		flash("New Restaurant Created...!!!")
+		return redirect(url_for('showRestaurants'))
+	else:
+		return render_template('newRestaurant.html' , restaurants = restaurants)
 
 #Edit an Existing Restaurant Page...
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-	return "This page will be for editing restaurant"
+	#return "This page will be for editing restaurant"
+	return render_template('editRestaurant.html' , i = editedRestaurant)
 
 #Delete an Existing Restaurant Page...
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-	return "This page will be for deleting restaurant"
+	#return "This page will be for deleting restaurant"
+	return render_template('deleteRestaurant.html' , i = deletedRestaurant)
 
 
 #Restaurant Menu...
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-	return "This page is the menu for restaurant"
-
+	#return "This page is the menu for restaurant"
+	return render_template('menu.html' , restaurant_id = restaurant_id)
 #Add a New Menu Item...
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-	return "This page is for creating new menu item for restaurant"
-
+	#return "This page is for creating new menu item for restaurant"
+	return render_template('newMenuItem.html' , restaurant_id = restaurant_id)
 #Edit an Existing Menu Item...
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-	return "This page is for editing menu item"
-
+	#return "This page is for editing menu item"
+	return render_template('editMenuItem.html' , i = editedItem)
 #Delete an Existing Menu Item...
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-	return "This page is for deleting menu item"
-
+	#return "This page is for deleting menu item"
+	return render_template('deleteMenuItem.html' , i = deletedItem)
 
 if __name__ == '__main__':
 	app.debug = True
